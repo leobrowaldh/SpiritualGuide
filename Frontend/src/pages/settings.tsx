af5c00875c757703f6def47cf62e15cf3f8d5c34
Leo Browaldh <leobrowaldh@gmail.com>
2025-09-01 20:22:39 +0200

    removed reference to unused icon

--- Frontend/src/pages/settings.tsx
+++ Frontend/src/pages/settings.tsx
@@ -1,12 +1,11 @@
-import settingsIcon from '../assets/settings-icon.svg';
 
-export default function Settings(){
-  function handleSettingsClick() {
-    console.log("Settings clicked");
-  }
-  return(
-    <button>
-      <img src={settingsIcon} alt="settings icon" className="w-8 h-8" onClick={handleSettingsClick} />
-    </button>
-  )
-}
+// export default function Settings(){
+//   function handleSettingsClick() {
+//     console.log("Settings clicked");
+//   }
+//   return(
+//     <button>
+//       <img src={settingsIcon} alt="settings icon" className="w-8 h-8" onClick={handleSettingsClick} />
+//     </button>
+//   )
+// }
